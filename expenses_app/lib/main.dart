c28db16cec7240cab6ff0f550f4a39934011d9c5
Marco Caldera <marco.caldera@one.fit>
2022-08-16 17:51:03 +0200

    Start expenses app

---
+++ expenses_app/lib/main.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: unnecessary_const
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: const Card(
+              elevation: 5,
+              color: Colors.blue,
+              child: Text("CHART"),
+            ),
+          ),
+          const Card(
+            child: Text("List of transactions"),
+          ),
+        ],
+      ),
+    );
+  }
+}
