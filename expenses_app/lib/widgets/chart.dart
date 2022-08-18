e27dd14ed5e5a4fae2293177a055bdbd1eebef59
Marco Caldera <marco.caldera@one.fit>
2022-08-18 23:28:27 +0200

    Add chart

--- expenses_app/lib/widgets/chart.dart
+++ expenses_app/lib/widgets/chart.dart
@@ -2,6 +2,8 @@ import 'package:expenses_app/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import 'chart_bar.dart';
+
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
@@ -13,27 +15,47 @@ class Chart extends StatelessWidget {
       double totalSum = 0.0;
 
       for (var tx in recentTransactions) {
-        if (tx.date.day == weekDay.day &&
-            tx.date.month == weekDay.month &&
-            tx.date.year == weekDay.year) {
+        if (hasHappenedThisDay(tx, weekDay)) {
           totalSum += tx.amount;
         }
       }
 
       return {
-        'day': DateFormat.E().format(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
     });
   }
 
+  double get totalSpending {
+    // reduce function
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
+    });
+  }
+
+  bool hasHappenedThisDay(Transaction tx, DateTime weekDay) {
+    return tx.date.day == weekDay.day &&
+        tx.date.month == weekDay.month &&
+        tx.date.year == weekDay.year;
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((data) {
+          return ChartBar(
+            data['day'] as String,
+            data['amount'] as double,
+            totalSpending == 0.0
+                ? 0
+                : (data['amount'] as double) / totalSpending,
+          );
+        }).toList(),
       ),
     );
   }
